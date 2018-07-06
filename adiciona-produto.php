42ce39e5e520e8f9736e565729efe26ca6ae68e0
leogif <leogif@gmail.com>
2018-07-06 17:07:01 -0300

    corrigindo conn

--- adiciona-produto.php
+++ adiciona-produto.php
@@ -1,12 +1,13 @@
 <?php 
-require_once('cabecalho.php'); require_once('conn.php');
-  $nome = $_REQUEST["nome"];
-  $preco = $_REQUEST["preco"];              
-  $query = "insert into produtos (nome, preco) values ($nome, $preco)";
-  mysqli_query($conn, $query) or die("Erro ao tentar cadastrar registro"); 
-  mysqli_close($conn);
+    require_once('cabecalho.php'); 
+    require_once('conn.php');
+    $nome = $_REQUEST["nome"];
+    $preco = $_REQUEST["preco"];              
+    $query = "insert into produtos (nome, preco) values ($nome, $preco)";
+    mysqli_query($conn, $query) or die("Erro ao tentar cadastrar registro"); 
+    mysqli_close($conn);
 ?>            
             <p class="alert-success">Produto <?= $nome; ?> <?= $preco; ?> adicionado com sucesso!</p>
         </header>
     </section>
-<?php include('rodape.php'); ?>
+<?php include('rodape.php
